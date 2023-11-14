84cc1ac34423e711a86925a3c3ce99715bede08a
Fehtor <f.truhachiov@mail.ru>
2023-11-14 19:34:32 +0300

    thirdUppdate

--- Test/test.js
+++ Test/test.js
@@ -1,6 +1,4 @@
 let cards = document.querySelectorAll(".card")
-let input = document.querySelector(".search-text")
-console.log(input)
 
 function jaro_distance(s1, s2)
     {
@@ -112,15 +110,24 @@ function jaro_Winkler(s1, s2)
         return jaro_dist.toFixed(6);
     }
 
-function changeState(){
-    let name = input.value
-    if(name == ""){
-        defaultState()
-        return
-    }
+
+
+function hideCard(name){
     for(let i = 0; i < cards.length; i++){
         let cardName = cards[i].getAttribute("name")
-        if(jaro_Winkler(name, cardName) >= 0.75){
+        if(jaro_Winkler(cardName, name) >= 0.75){
+            cards[i].classList.add("display-none")
+        }
+        else{
+            cards[i].classList.remove("display-none")
+        }
+    }
+}
+
+function showCard(name){
+    for(let i = 0; i < cards.length; i++){
+        let cardName = cards[i].getAttribute("name")
+        if(jaro_Winkler(cardName, name) >= 0.75){
             cards[i].classList.remove("display-none")
         }
         else{
@@ -129,10 +136,4 @@ function changeState(){
     }
 }
 
-function defaultState(){
-    for(let i = 0; i < cards.length; i++){
-        cards[i].classList.remove("display-none")
-    }
-}
 
-input.addEventListener("input", changeState)
